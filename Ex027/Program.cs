f40052b777d7309dead8a10605119d2647432957
Liubov.V.B <griark0611@gmail.com>
2022-11-28 13:07:08 +0300

    add Ex027

---
+++ Ex027/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNum(Num));
+
+int SumNum(int Num)
+{
+    int Sum = 0;
+    int i = 0;
+    while (Num > 0)
+    {
+        i = Num % 10;
+        Sum += i;
+        Num /= 10;
+    }  
+    return Sum;  
+}
